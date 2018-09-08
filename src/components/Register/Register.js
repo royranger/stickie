d5191d8b63f0abe92dcf37976d8eb4225a18deaf
Joy Goh-Mah <royran9er@gmail.com>
2018-09-09 00:10:32 +0100

    adds validation for register

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -48,9 +48,19 @@ class Register extends Component {
       if(data.id) {
         loadUser(data);
         routeBoard();
+      } else if (data === 'fields empty') {
+        window.alert('Please fill out all fields.');
+      } else {
+        window.alert('User already exists');
       }
     })
 
+  }
+
+  handleEnter = (event) => {
+    if (event.key === "Enter") {
+      this.onSubmitRegister();
+    }
   }
 
 
@@ -64,11 +74,14 @@ class Register extends Component {
           <div id="registerbox" className="ma3 pa3 ba b--yellow shadow-4">
             <div className="tc">
               <label>Name </label><br/>
-              <input type="text" onChange={this.onNameChange}/><br/><br/>
+              <input type="text" onChange={this.onNameChange}
+                                  onKeyPress={this.handleEnter}/><br/><br/>
               <label>Username</label><br/>
-              <input type="text" onChange={this.onUsernameChange}/><br/><br/>
+              <input type="text" onChange={this.onUsernameChange}
+                                  onKeyPress={this.handleEnter}/><br/><br/>
               <label>Password</label><br/>
-              <input type="password" onChange={this.onPasswordChange} /><br/><br/>
+              <input type="password" onChange={this.onPasswordChange}
+                                      onKeyPress={this.handleEnter}/><br/><br/>
                 <div className="pointer f6 grow no-underline br-pill ba bw1 ph3 pv2 mb2 dib hot-pink"
                                 onClick={this.onSubmitRegister}>Create board</div>
                 <p>Already have a board? <span className="hot-pink underline pointer"
